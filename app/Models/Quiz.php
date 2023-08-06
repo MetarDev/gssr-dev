7bde49686ade1ebd9078b03dcb62600275a9afa7
Ivan Grginov <ivangrginovbiz@gmail.com>
2023-08-06 23:21:19 +0200

    Create quiz model

---
+++ app/Models/Quiz.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Quiz extends Model
+{
+    use HasFactory;
+
+    /**
+     * Guarded properties that can't be saved over.
+     *
+     * @var array<string>
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * Casting some Model attributes
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'questions' => 'array',
+    ];
+}
