cb37aa290971ce4a3e1be4d82f473df0bd19d8e3
Ivan Grginov <ivangrginovbiz@gmail.com>
2023-08-12 13:42:52 +0200

    Added CopyToClipboard component

---
+++ resources/js/Components/CopyToClipboard.tsx
@@ -0,0 +1,28 @@
+import { IconButton, Tooltip, useClipboard } from '@chakra-ui/react';
+import { LinkIcon, CheckIcon } from '@chakra-ui/icons';
+import { useState } from 'react';
+
+export default function CopyToClipboard({
+  toCopy,
+  subject = "URL"
+}: {
+  toCopy: string
+  subject?: string
+}) {
+  const { onCopy, hasCopied } = useClipboard(toCopy);
+
+  const copyLabel = hasCopied ? `Copied ${subject} to clipboard!` : `Copy ${subject} to clipboard`;
+
+    return (
+      <Tooltip label={copyLabel} fontSize='md' placement='top'>
+        <IconButton
+          aria-label={`Copy ${subject} to clipboard`}
+          variant={"outline"}
+          colorScheme={hasCopied ? "green" : 'orange'}
+          onClick={onCopy}
+          disabled={hasCopied}
+          icon={hasCopied ? <CheckIcon />: <LinkIcon />}
+        />
+      </Tooltip>
+    );
+}
