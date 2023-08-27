dfcd67b1f54786d2cc90949fbd0c963d11ab53aa
Ivan Grginov <ivangrginovbiz@gmail.com>
2023-08-27 12:38:55 +0200

    Add some highlighting to questions

--- resources/js/Components/Quiz/QuestionTitle.tsx
+++ resources/js/Components/Quiz/QuestionTitle.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Heading } from "@chakra-ui/react";
+import { Heading, Code } from "@chakra-ui/react";
 import { QuestionInterface } from "@/types/quiz";
+import HighlightSupports from "./HighlightSupports";
 
 const QuestionTitle = ({
 	question,
@@ -27,7 +28,7 @@ const QuestionTitle = ({
 
 			return (
 				<Heading as="h1" marginBottom={6}>
-					Which {featureSupportData.subjectType.toUpperCase()} feature {featureSupportData.isSupported ? 'IS' : 'IS NOT '} supported by {featureSupportData.browserName} {featureSupportData.browserVersion} ({featureSupportData.browserYear})
+					Which {featureSupportData.subjectType.toUpperCase()} feature <HighlightSupports condition={featureSupportData.isSupported} supportsText="IS" notSupportsText="IS NOT" /> supported by <Code style={{ fontSize: "inherit"}}>{featureSupportData.browserName} {featureSupportData.browserVersion}</Code> ({featureSupportData.browserYear})
 				</Heading>
 			);
 		case "browser_support":
@@ -37,7 +38,7 @@ const QuestionTitle = ({
 
 			return (
 				<Heading as="h1" marginBottom={6}>
-					Which {browserSupportData.subjectType} browser {browserSupportData.isSupported ? 'SUPPORTS' : 'DOES NOT SUPPORT'} {browserSupportData.featureShortName} {`(${browserSupportData.featureFullName})`}
+					Which {browserSupportData.subjectType} browser <HighlightSupports condition={browserSupportData.isSupported} supportsText="SUPPORTS" notSupportsText="DOES NOT SUPPORT" /><Code style={{ fontSize: "inherit"}}>{browserSupportData.featureShortName}</Code> {`(${browserSupportData.featureFullName})`}
 				</Heading>
 			);
 		case "usage_global":
@@ -47,7 +48,7 @@ const QuestionTitle = ({
 
 			return (
 				<Heading as="h1" marginBottom={6}>
-					Which {globalUsageData.subjectType.toUpperCase()} feature is {globalUsageData.isMostUsed ? 'MOST' : 'LEAST'} globally supported?
+					Which {globalUsageData.subjectType.toUpperCase()} feature is <HighlightSupports icons="up-down" condition={globalUsageData.isMostUsed} supportsText="MOST" notSupportsText="LEAST" /> globally supported?
 				</Heading>
 			);
 		default:
