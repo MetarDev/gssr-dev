f1b3e6c4205b62ecbb2b2ee463f51c2a155bd0ad
Ivan Grginov <ivangrginovbiz@gmail.com>
2023-08-31 16:16:54 +0200

    Better CTA layout

--- resources/js/Components/Quiz/ShareQuizCta.tsx
+++ resources/js/Components/Quiz/ShareQuizCta.tsx
@@ -28,32 +28,58 @@ const ShareQuizCta = ({
   const { colorMode } = useColorMode();
   return (
     <Card
-      padding={{ base: 8, md: 16 }}
+      padding={{ base: 8, md: 12, lg: 16 }}
       bg={colorMode === "dark" ? "green.700" : "green.200"}
       width={"full"}
     >
-      <VStack
-        align={"center"}
-        spacing={8}
+      <Flex
+        gap={{base: 8, lg: 16}}
+        justifyContent={{ base: "flex-start", md: "space-between"}}
+        flexDirection={{ base: "column", md: "row" }}
         alignItems={"center"}
-        justifyContent={"center"}
       >
-        <Heading alignSelf={{ base: "flex-start", sm: "center" }}>Challenge Your Peers!</Heading>
-        <Text color={"brandTextDefault"} alignSelf={{ base: "flex-start", sm: "center" }} maxW={"2xl"}>
-          Don't keep your quiz success to yourself! Flaunt your guessing-game by
-          sharing your quiz accomplishment with your social circle and inspire
-          others to take the challenge.
-        </Text>
-
-        <SimpleGrid spacing={{base: 4, md: 8 }} columns={3}>
+        <VStack
+          align={"center"}
+          spacing={8}
+          alignItems={"flex-start"}
+          justifyContent={"center"}
+        >
+          <Heading>
+            Challenge Your Peers!
+          </Heading>
+          <Text
+            color={"brandTextDefault"}
+            alignSelf={{ base: "flex-start", sm: "center" }}
+            maxW={"2xl"}
+          >
+            Don't keep your quiz success to yourself! Flaunt your guessing-game
+            by sharing your quiz accomplishment with your social circle and
+            inspire others to take the challenge.
+          </Text>
+        </VStack>
+        <SimpleGrid spacing={{ base: 4, sm: 8 }} columns={3} flexShrink={0}>
           <TwitterShareIcon quizUrl={url} quizSummary={quizSummary} size="lg" />
-          <WhatsappShareIcon quizUrl={url} quizSummary={quizSummary} size="lg" />
+          <WhatsappShareIcon
+            quizUrl={url}
+            quizSummary={quizSummary}
+            size="lg"
+          />
           <FacebookShareIcon quizUrl={url} size="lg" />
           <LinkedinShareIcon quizUrl={url} size="lg" />
-          <MastodonShareIcon quizUrl={url} quizSummary={quizSummary} size="lg" />
-          <CopyToClipboard toCopy={url} subject="Quiz URL" size="lg" variant={"solid"} marginLeft={0} />
+          <MastodonShareIcon
+            quizUrl={url}
+            quizSummary={quizSummary}
+            size="lg"
+          />
+          <CopyToClipboard
+            toCopy={url}
+            subject="Quiz URL"
+            size="lg"
+            variant={"solid"}
+            marginLeft={0}
+          />
         </SimpleGrid>
-      </VStack>
+      </Flex>
     </Card>
   );
 };
