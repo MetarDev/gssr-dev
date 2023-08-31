1aba25270659cb99eb57a32c282164bc61fd0021
Ivan Grginov <ivangrginovbiz@gmail.com>
2023-08-31 17:03:50 +0200

    Fixing mock data a bit

--- resources/js/Pages/Quiz.tsx
+++ resources/js/Pages/Quiz.tsx
@@ -44,7 +44,7 @@ export default function QuizPage({
       {
         timeSpent: 23,
         score: 0,
-        answerId: 4,
+        answerId: 253,
         answeredCorrectly: false,
       },
       {
@@ -65,13 +65,49 @@ export default function QuizPage({
         answerId: 5,
         answeredCorrectly: false,
       },
+      {
+        timeSpent: 23,
+        score: 0,
+        answerId: 59,
+        answeredCorrectly: false,
+      },
+      {
+        timeSpent: 23,
+        score: 120,
+        answerId: 128,
+        answeredCorrectly: true,
+      },
+      {
+        timeSpent: 23,
+        score: 123,
+        answerId: 67,
+        answeredCorrectly: true,
+      },
+      {
+        timeSpent: 12,
+        score: 0,
+        answerId: 120,
+        answeredCorrectly: false,
+      },
+      {
+        timeSpent: 23,
+        score: 123,
+        answerId: 1,
+        answeredCorrectly: true,
+      },
+      {
+        timeSpent: 12,
+        score: 0,
+        answerId: 122,
+        answeredCorrectly: false,
+      },
     ],
-    correctQuestions: 2,
-    totalQuestions: 4,
-    timeSpent: 81,
-    timeTotal: 120,
+    correctQuestions: 5,
+    totalQuestions: 10,
+    timeSpent: 167,
+    timeTotal: 30 * 10,
     avgTimePerQuestion: 19.99,
-    maxScore: 200 * 4
+    maxScore: 200 * 10
   };
 
   const test = true;
@@ -105,6 +141,7 @@ export default function QuizPage({
       // {hasEnded &&
         <QuizSummary
           quiz={quiz}
+          questions={questions}
           quizSummary={mockQuizSummary}
           url={url}
         />
