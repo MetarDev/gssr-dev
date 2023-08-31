8df1e75e8a34efde95084bf41bcba1f169b2ce1a
Ivan Grginov <ivangrginovbiz@gmail.com>
2023-08-31 16:01:13 +0200

    Modified the handling of icons

--- resources/js/Components/CopyToClipboard.tsx
+++ resources/js/Components/CopyToClipboard.tsx
@@ -1,15 +1,21 @@
 import { IconButton, Tooltip, useClipboard } from "@chakra-ui/react";
 import { LinkIcon, CheckIcon } from "@chakra-ui/icons";
 import { useState } from "react";
+import { IconSize } from "@/types/icon-size";
+import { getIconSize } from "@/Helpers/icons";
 
 export default function CopyToClipboard({
   toCopy,
   subject = "URL",
   marginLeft = 2,
+  size = 'md',
+  variant = 'outline',
 }: {
   toCopy: string;
   subject?: string;
   marginLeft?: number;
+  size: IconSize,
+  variant?: 'outline' | 'solid';
 }) {
   const { onCopy, hasCopied } = useClipboard(toCopy);
 
@@ -17,17 +23,19 @@ export default function CopyToClipboard({
     ? `Copied ${subject} to clipboard!`
     : `Copy ${subject} to clipboard`;
 
+  const {iconButtonSize, chakraIconSize} = getIconSize(size);
+
   return (
     <Tooltip label={copyLabel} fontSize="md" placement="top" closeOnClick={false}>
       <IconButton
+        boxSize={iconButtonSize}
         marginLeft={marginLeft}
         aria-label={`Copy ${subject} to clipboard`}
-        variant={"outline"}
+        variant={variant}
         colorScheme={hasCopied ? "green" : "orange"}
         onClick={onCopy}
         disabled={hasCopied}
-        icon={hasCopied ? <CheckIcon /> : <LinkIcon />}
-
+        icon={hasCopied ? <CheckIcon boxSize={chakraIconSize} /> : <LinkIcon boxSize={chakraIconSize} />}
       />
     </Tooltip>
   );
