43034f8b705afb5839eaf9881abdf2d399514053
Ivan Grginov <ivangrginovbiz@gmail.com>
2023-09-02 11:23:53 +0200

    Add cookie policy page

---
+++ resources/js/Pages/CookiePolicy.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@chakra-ui/react";
+import BasicPageLayout from "@/Layouts/BasicPageLayout";
+
+export default function CookiePolicy() {
+  return (
+    <BasicPageLayout title="Cookie Policy">
+      <Text>Our cookie policy is really simple! We don't use any non-functional cookies.</Text>
+    </BasicPageLayout>
+  );
+}
