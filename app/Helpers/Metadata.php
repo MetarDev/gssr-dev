83b922f018cf6a8a0f3cfbd9546e5987a0d87805
Ivan Grginov <ivangrginovbiz@gmail.com>
2023-09-04 12:18:25 +0200

    Setting up social media featured images

--- app/Helpers/Metadata.php
+++ app/Helpers/Metadata.php
@@ -6,13 +6,34 @@ use Illuminate\Support\Facades\Request as FacadesRequest;
 
 class Metadata
 {
+
+    /**
+     * Generates the page metadata.
+     *
+     * @param array $metadata
+     * @param string $metadata.title
+     * @param string $metadata.description
+     * @param string $metadata.image
+     * @param string $metadata.url
+     * @return array
+     */
     public static function generatePageMetadata(array $metadata): array
     {
         return [
             'title' => $metadata['title'] ?? '',
             'description' => $metadata['description'] ?? '',
-            'image' => $metadata['image'] ?? '',
+            'image' => $metadata['image'] ?? self::getDefaultImage(),
             'url' => FacadesRequest::url(),
         ];
+    }
+
+    /**
+     * Get the default image.
+     *
+     * @return string
+     */
+    public static function getDefaultImage(): string
+    {
+        return Images::resolve('default-1200x630.png');
     }
 }
