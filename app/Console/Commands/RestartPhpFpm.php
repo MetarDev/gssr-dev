fad9c02d6c58a5f1724901fa9337b911732cc17a
Ivan Grginov <ivangrginovbiz@gmail.com>
2023-09-04 22:20:10 +0200

    Updated command for clering php-fpm

--- app/Console/Commands/RestartPhpFpm.php
+++ app/Console/Commands/RestartPhpFpm.php
@@ -25,9 +25,76 @@ class RestartPhpFpm extends Command
      */
     public function handle()
     {
-        $service = new \Cloudways\Server\Service\Service();
-        $service->SetEmail(env('CLOUDWAYS_EMAIL'));
-        $service->SetKey(env('CLOUDWAYS_API_KEY'));
-        $service->manageServices(env('CLOUDWAYS_SERVER_ID'), 'php8.1-fpm', 'restart');
+        $this->callCloudwaysAPI('POST', '/service/state', $this->fetchAccessToken(), [
+            'server_id' => env('CLOUDWAYS_SERVER_ID'),
+            'service' => 'php8.1-fpm',
+            'state' => 'restart'
+        ]);
+    }
+
+    /**
+     * Fetches the access token from Cloudways.
+     *
+     * @return string
+     */
+    private function fetchAccessToken(): string
+    {
+        //Fetch Access Token
+        $tokenResponse = $this->callCloudwaysAPI(
+            'POST',
+            '/oauth/access_token',
+            null,
+            [
+                'email' => env('CLOUDWAYS_EMAIL'),
+                'api_key' => env('CLOUDWAYS_API_KEY'),
+            ]
+        );
+
+        return $tokenResponse->access_token;
+    }
+
+    /**
+     * Calls the Cloudways API.
+     *
+     * @param string $method
+     * @param string $url
+     * @param string|null $accessToken
+     * @param array $post
+     * @return object
+     */
+    private function callCloudwaysAPI($method, $url, $accessToken, $post = [])
+    {
+        $baseURL = 'https://api.cloudways.com/api/v1';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($ch, CURLOPT_URL, $baseURL . $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        //curl_setopt($ch, CURLOPT_HEADER, 1);
+        //Set Authorization Header
+        if ($accessToken) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer ' . $accessToken]);
+        }
+
+        //Set Post Parameters
+        $encoded = '';
+        if (count($post)) {
+            foreach ($post as $name => $value) {
+                $encoded .= urlencode($name) . '=' . urlencode($value) . '&';
+            }
+            $encoded = substr($encoded, 0, strlen($encoded) - 1);
+
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $encoded);
+            curl_setopt($ch, CURLOPT_POST, 1);
+        }
+
+        $output = curl_exec($ch);
+
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if ($httpcode != '200') {
+            die('An error occurred code: ' . $httpcode . ' output: ' . substr($output, 0, 10000));
+        }
+        curl_close($ch);
+        return json_decode($output);
     }
 }
