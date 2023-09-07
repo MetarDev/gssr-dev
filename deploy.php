ac71c0ea32fead322ffe0c3eeb8329e48e927ab4
Ivan Grginov <ivangrginovbiz@gmail.com>
2023-09-07 12:16:58 +0200

    Renamed repo

--- deploy.php
+++ deploy.php
@@ -5,7 +5,7 @@ require 'recipe/laravel.php';
 
 // Config
 
-set('repository', 'git@github.com:MetarDev/support-guesser.git');
+set('repository', 'git@github.com:MetarDev/gssr-dev.git');
 
 set('keep_releases', 7);
 
